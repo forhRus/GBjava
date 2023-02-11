faece97383ca72a1b1e257719e68df13d3ef8ddd
Sergey <shkaev.ss@mail.ru>
2023-02-11 19:22:29 +0300

    task1 complete

--- task1/src/Main.java
+++ task1/src/Main.java
@@ -1,5 +1,21 @@
+//Написать программу вычисления n-ого треугольного числа.
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private static Scanner input = new Scanner(System.in);
+    public static void main(String[]args){
+        int number = getNumberByUser("Введите номер триугольного числа: ");
+        double triangularNumber = getTriangularNumber(number);
+        System.out.printf("Число T%d = %.0f", number, triangularNumber);
+    }
+    //принимаем число
+    public static int getNumberByUser(String text){
+        System.out.print(text);
+        return input.nextInt();
+    }
+    //определяем триугольное число
+    public static double getTriangularNumber(int n){
+        double a = 0.5 * n * (n + 1);
+//        int a = (n + 1) * n / 2;
+        return  a;
     }
 }
