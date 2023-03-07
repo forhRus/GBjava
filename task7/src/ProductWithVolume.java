00b93a7a09d9d844cb3159ef3fd91f482f956f20
Sergey <shkaev.ss@mail.ru>
2023-03-07 14:21:56 +0300

    создан клас продуктов с объёмом

---
+++ task7/src/ProductWithVolume.java
@@ -0,0 +1,12 @@
+public class ProductWithVolume extends Goods{
+    protected int volume;
+
+    public ProductWithVolume(String name, double price, int quantity, String unit, int volume) {
+        super(name, price, quantity, unit);
+        this.volume = volume < 0 ? 0 : volume;
+    }
+
+    public String toString() {
+        return String.format("%s, Объём: %d л.", super.toString(), this.volume);
+    }
+}
