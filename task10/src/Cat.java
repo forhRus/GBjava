f2ef4c86cc47b3743a0d63b02cbd55bf8a6b5d9d
Sergey <shkaev.ss@mail.ru>
2023-03-18 20:58:29 +0300

    equals для cat и hashcode

--- task10/src/Cat.java
+++ task10/src/Cat.java
@@ -10,6 +10,7 @@ public class Cat {
         this.color = color;
         this.name = name;
     }
+
     public Cat() {
         this("Барсик", 200, 5, 2, "серый");
     }
@@ -48,17 +49,32 @@ public class Cat {
 
     @Override
     public boolean equals(Object obj) {
-        if(obj instanceof Cat) {
-            Cat t = (Cat)obj;
-            return t.name == this.name
-            && t.age == this.age
-            && t.color == this.color
-            && t.height == this.height
-            && t.weight == this.weight;
-        } else
+        if (obj == this)
+            return true;
+        if (obj == null || !(obj instanceof Cat))
             return false;
+        Cat cat = (Cat) obj;
+        if (cat.age != this.age
+                || cat.height != this.height
+                || cat.weight != this.weight)
+            return false;
+        if (this.name == null) {
+            if (this.color == null) {
+                return cat.color == null && cat.name == null;
+            } else {
+                return this.color.equals(cat.color) && cat.name == null;
+            }
+        } else if (this.color == null) {
+            return this.name.equals(cat.color) && cat.color == null;
+        } else {
+            return this.name.equals(cat.name) && this.color.equals(cat.color);
+        }
     }
+
+    @Override
     public int hashCode() {
-        return  weight;
+        int sum = this.name.length() * 123 + this.color.length() * 123 +
+                + this.weight * 123 + this.age * 123 + this.height * 123;
+        return sum;
     }
 }
