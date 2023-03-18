ae7c3a8c16f719fc4deb5c016106312b5fcefdd9
Sergey <shkaev.ss@mail.ru>
2023-03-18 14:04:59 +0300

    Вывод массива на экран

--- task10/src/MyList.java
+++ task10/src/MyList.java
@@ -1,42 +1,39 @@
-import java.util.ArrayList;
-
 public class  MyList <T>{
-    private Object[] arr;
+    private Object[] list;
     final private int DEFAULT_LENGTH = 10;
     final private Object[] EMPTY_ARRAY = {};
     private int size;
 
     public MyList(int length) {
         if(length > 0) {
-            this.arr = new Object[length];
+            this.list = new Object[length];
         } else if (length == 0) {
-            this.arr = new Object[DEFAULT_LENGTH];
+            this.list = new Object[DEFAULT_LENGTH];
         } else {
             System.out.println("Длина меньше нуля. Пока не умею обрабатывать ошибки!");
         }
     }
 
-    public MyList(T[] arr) {
-        size = arr.length;
-        this.arr = new Object[(int)(size*1.5)];
-        for (int i = 0; i < size; i++) {
-            this.arr[i] = arr[i];
-        }
-    }
+//     public MyList(List<Object> list) {
+//         for (int i = 0; i < list.size(); i++) {
+//             add(list.get(i));
+//         }
+//    }
 
     public MyList() {
-        this.arr = new Object[DEFAULT_LENGTH];
+        this.list = new Object[DEFAULT_LENGTH];
         this.size = 0;
     }
 
-    public void add(T obj) {
+    public void add(Object obj) {
         if(obj != null) {
-            arr[size] = obj;
+            list[size] = obj;
             size++;
         } else {
             System.out.println("Нельзя добавить null");
         }
     }
+
 //    public void add(int index, T obj) {
 //        if(checkIndex(index)) {
 //            this.customArr.add(index, obj);
@@ -86,34 +83,34 @@ public class  MyList <T>{
 //        return false;
 //    }
 //
-//    @Override
-//    public String toString() {
-//        StringBuilder res = new StringBuilder("[");
-//        if(customArr.size() > 0) {
-//            res.append(elementToString(customArr.get(0)));
-//        }
-//        for (int i = 1; i < customArr.size(); i++) {
-//            res.append(", ");
-//            res.append(elementToString(customArr.get(i)));
-//        }
-//        res.append("]");
-//        return res.toString();
-//    }
-//    private String elementToString(T obj) {
-//        String res = "";
-//        if(obj instanceof Integer || obj instanceof Double){
-//            return obj.toString();
-//        } else if (obj instanceof Character) {
-//            return String.format("'%s'", obj);
-//        }else if (obj instanceof String) {
-//            return String.format("\"%s\"", obj);
-//        }else if (obj instanceof Cat) {
-//            Cat cat = (Cat) obj;
-//            return cat.toString();
-//        } else {
-//            return null;
-//        }
-//    }
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder("[");
+        if(this.size > 0) {
+            result.append(elementToString(this.list[0]));
+        }
+        for (int i = 1; i < this.size; i++) {
+            result.append(", ");
+            result.append(elementToString(this.list[i]));
+        }
+        result.append("]");
+        return result.toString();
+    }
+    private String elementToString(Object obj) {
+        String res = "";
+        if(obj instanceof Integer || obj instanceof Double){
+            return obj.toString();
+        } else if (obj instanceof Character) {
+            return String.format("'%s'", obj);
+        }else if (obj instanceof String) {
+            return String.format("\"%s\"", obj);
+        }else if (obj instanceof Cat) {
+            Cat cat = (Cat) obj;
+            return cat.toString();
+        } else {
+            return null;
+        }
+    }
 //    private boolean checkIndex(int i) {
 //        if (i > -1 && i < customArr.size() )
 //            return true;
--- task10/src/Program.java
+++ task10/src/Program.java
@@ -1,13 +1,29 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.math.RoundingMode;
 
 public class Program {
     public static void main(String[] args) {
-        MyList<Integer> myList = new MyList<>();
-        myList.add(123);
-        myList.add(22);
-        myList.add(33);
-        System.out.println(myList.size());
+        int textLength = 15;
+        MyList<Object> myList1 = new MyList<>(textLength);
+        for (int i = 0; i < textLength; i++) {
+            if (i % 4 == 0) {
+                double a = (double) ((int) (i * 2.12 * 100)) / 100;;
+                myList1.add(a);
+            } else if (i % 3 == 0) {
+                myList1.add(new Cat());
+            } else if (i % 2 == 0) {
+                myList1.add(String.format("Строка %d", i));
+            } else {
+                myList1.add(i);
+            }
+        }
+        System.out.println(myList1);
+
+
+        MyList<Object> myList2 = new MyList<>();
+        myList2.add(123);
+        myList2.add(22);
+        myList2.add(33);
+        System.out.println(myList2.size());
 
 
     }
